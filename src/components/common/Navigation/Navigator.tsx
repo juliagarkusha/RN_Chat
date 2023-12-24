bffa467cb318ca93a0a5bed01d99f11ff27aa9b7
magna <julijagarkusha@gmail.com>
2023-12-24 11:39:10 +0200

    added domashka

--- src/components/common/Navigation/Navigator.tsx
+++ src/components/common/Navigation/Navigator.tsx
@@ -1,15 +1,18 @@
 // External deps
 import React, { FC } from "react";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { BlurView } from 'expo-blur';
 
 // Internal deps
 import ChatsProvider from "../../../contexts/Chats";
 import ThemeProvider from "../../../contexts/Theme";
 import TabsNavigator from "./Tabs/TabsNavigator";
 import useTheme from "../../../hooks/useTheme";
+import useAppState from "../../../hooks/useAppState";
 
 const Navigator: FC = () => {
-    const { colors } = useTheme();
+    const { colors, blurContainer } = useTheme();
+    const appStateCurrent = useAppState();
 
     const MyTheme = {
         ...DefaultTheme,
@@ -27,6 +30,10 @@ const Navigator: FC = () => {
             <ChatsProvider>
                 <NavigationContainer theme={MyTheme}>
                     <TabsNavigator />
+                    {appStateCurrent !== 'active'
+                        ? <BlurView intensity={30} tint="dark" style={blurContainer} />
+                        : null
+                    }
                 </NavigationContainer>
             </ChatsProvider>
         </ThemeProvider>
